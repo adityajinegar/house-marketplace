68e83b6af9a131f1b3d4f40d5702aaf33ace9c9b
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-02-09 20:47:54 -0500

    pages and routes

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFireStore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXUDA4ZZt81zEqExoqJDUoc55pirfWma0',
+  authDomain: 'house-marketplace-app-74d87.firebaseapp.com',
+  projectId: 'house-marketplace-app-74d87',
+  storageBucket: 'house-marketplace-app-74d87.appspot.com',
+  messagingSenderId: '61587400263',
+  appId: '1:61587400263:web:10ddffe81893b1d1cab362',
+  measurementId: 'G-S5XWNWR4DP',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
